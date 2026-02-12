5731347c66ed09c1f06fd282ea6a0f9c9db11c0b
Jerin Prakash <jerinprakash892@gmail.com>
2026-02-12 23:31:39 +0530

    code added

--- src/GarbageCollection.java
+++ src/GarbageCollection.java
@@ -1,17 +1,20 @@
 public class GarbageCollection {
-    
-    // TODO: Override the finalize() method
-    // protected void finalize()
-    // Inside it, print "Garbage Collected"
+
+    // Overriding finalize method
+    @Override
+    protected void finalize() {
+        System.out.println("Garbage Collected");
+    }
 
     public static void main(String[] args) {
-        // TODO: Create an object of GarbageCollection class
-        
-        // TODO: Make the object eligible for garbage collection (e.g., assign null)
-        
-        // TODO: Request Garbage Collection using System.gc()
-        
-        // Tip: You might need a small delay or simply exiting might trigger it in some JVMs, 
-        // but System.gc() is the standard call.
+
+        // Creating object
+        GarbageCollection obj = new GarbageCollection();
+
+        // Making object eligible for GC
+        obj = null;
+
+        // Requesting Garbage Collector
+        System.gc();
     }
 }
